bc3f2a852acff614d366985668edfc4ae0eb9c77
kapilbindal <kapil.bindal98@gmail.com>
2018-10-19 20:09:59 +0530

    Face recognizer added

--- faceDataCollect.py
+++ faceDataCollect.py
@@ -10,37 +10,34 @@ dataset_path = './data/'
 file_name = input("Enter the name of the person : ")
 while True:
 	ret,frame = cam.read()
-	if(ret == False):
+	if ret == False:
 		continue
 	faces = face_cascade.detectMultiScale(frame,1.3,5)
 	if len(faces)==0:
 		continue
-	faces = sorted(faces,key=lambda f:f[2]*f[3],reverse=True)
-	for (x,y,w,h) in faces:
+	faces = sorted(faces,key=lambda f:f[2]*f[3])
+	for face in faces[-1:]:
+		x,y,w,h = face
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
 		offset = 10
 		face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
 		face_section = cv2.resize(face_section,(100,100))
 
 		skip += 1
-		if(skip % 10 == 0):
+		if(skip % 2 == 0):
 			face_data.append(face_section)
+			face_data_np = np.array(face_data)
+			face_data_np = face_data_np.reshape((face_data_np.shape[0],-1))
+			np.save(dataset_path+file_name+'.npy',face_data_np)
 			print(len(face_data))
-
 
 	cv2.imshow("Frame",frame)
 	cv2.imshow("Face Section",face_section)
 
 	key_pressed = cv2.waitKey(1) & 0xFF
-	if(key_pressed == ord('q')):
+	if key_pressed == ord('q'):
+		print("Data Successfully save at "+dataset_path+file_name+'.npy')
 		break
-
-face_data = np.asarray(face_data)
-face_data = face_data.reshape((face_data.shape[0],-1))
-print(face_data.shape)
-
-np.save(dataset_path+file_name+'.npy',face_data)
-print("Data Successfully save at "+dataset_path+file_name+'.npy')
 
 cam.release()
 cv2.destroyAllWindows()
